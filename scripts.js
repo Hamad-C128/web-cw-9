e48dfb01b50915284270185e941efba302d11bdb
Hamad-158 <Hamad.bfar@gmail.com>
2020-10-10 19:48:06 +0300

    done

--- scripts.js
+++ scripts.js
@@ -1,12 +1,40 @@
 // 1
 // moon.jpg قم بالوصول إلى الصورة وتغيرها إلى
 
+//   let img = document.getElementById('image');
+//   img.src = "../moon.jpg"
 // 2
 // "قم بالوصول إلى كلمة الأرض وتحويلها إلى "القمر
+
+// let text = document.getElementById('caption');
 
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+let mma = document.getElementById('caption');
+
+    document.querySelector('.card').addEventListener('click', () => {
+        if (mma.innerHTML == "الشمس") {
+        let tt = document.getElementById("caption");
+        tt.innerHTML = "القمر";
+        let image = document.getElementById("image");
+        image.src = "./images/moon.jpg"
+        let body = document.getElementById("body");
+        body.style.background = `#686d76`
+    
+}else if (mma.innerHTML == "القمر") {
+     
+        let tt = document.getElementById("caption");
+    tt.innerHTML = "الشمس";
+    let image = document.getElementById("image");
+    image.src = "./images/sun.jpg"
+    let body = document.getElementById("body");
+    body.style.background = `#fadcaa`
+    
+}
+
+})
+       
 
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
